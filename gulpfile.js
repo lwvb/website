40e562ab5aba2cabccd9804034e3ed3000b93164
Leendert van Beelen <commit@leendertvanbeelen.nl>
2017-03-24 23:23:37 +0200

    Removed html copy from gulp
    The html files are translated in templates that should be parsed, this
    gulp tasks shouldn't be used anymore

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,6 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
  
-gulp.task('html', function() {
-  return gulp.src('src/*.html')
-  .pipe(gulp.dest('dist/'));
-});
- 
-gulp.task('html:watch', ['html'], function () {
-  gulp.watch('./src/*.html', ['html']);
-});
-
 gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sourcemaps.init())
@@ -27,11 +18,10 @@ gulp.task('sass:watch', ['sass'], function () {
 });
 
 
-gulp.task('default', ['html','sass'], function () {
+gulp.task('default', ['sass'], function () {
 });
 
 gulp.task('default:watch', ['default'], function () {
   gulp.watch('./src/sass/**/*.scss', ['sass']);
-  gulp.watch('./src/*.html', ['html']);
 });
 
